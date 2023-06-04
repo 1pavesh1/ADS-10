6d4c07635ed7c26cc40668b6a2b33753b75e8440
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-06-04 23:37:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,7 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <vectr>
 #include  "tree.h"
 std::vector<char> getPerm(const Tree& tree, int n) {
   std::vector<std::vector<char>> permutations = tree.getPermutations();
