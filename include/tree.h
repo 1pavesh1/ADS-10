f155b4ab385f79b41139462b1cf8d7847a021102
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-06-04 23:49:39 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -46,7 +46,7 @@ class Tree {
         return *cha;
     }
  
- public:
+public:
     explicit Tree(std::vector<char> descendants) : root(nullptr) {
         root = Create(root, descendants);
         std::vector<char> ch;
